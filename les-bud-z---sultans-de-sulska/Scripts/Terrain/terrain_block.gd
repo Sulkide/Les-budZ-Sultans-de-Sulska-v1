d82d9f008985ac42c6b607683df94d9dac923976
Patcho <patcho.gif@gmail.com>
2025-11-05 13:23:02 +0100

    .

--- les-bud-z---sultans-de-sulska/Scripts/Terrain/terrain_block.gd
+++ les-bud-z---sultans-de-sulska/Scripts/Terrain/terrain_block.gd
@@ -1,9 +1,9 @@
 @tool
 extends StaticBody3D
 
-@export var size: Vector3 = Vector3.ONE:
+@export var block_size: Vector3 = Vector3.ONE:
 	set(value):
-		size = value
+		block_size = value
 		if is_node_ready():
 			_update_size()
 
@@ -11,5 +11,5 @@ func _ready() -> void:
 	_update_size()
 
 func _update_size() -> void:
-	$CollisionShape3D.shape.size = size
-	$MeshInstance3D.mesh.size = size
+	$CollisionShape3D.shape.size = block_size
+	$MeshInstance3D.mesh.size = block_size
